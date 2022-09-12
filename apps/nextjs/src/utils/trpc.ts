94356c9178b9c8fb757b8d1fd283b782c907a854
Erick Reis <erickcorreareis@gmail.com>
2022-09-11 21:00:41 -0300

    Initial commit

---
+++ apps/nextjs/src/utils/trpc.ts
@@ -0,0 +1,21 @@
+// src/utils/trpc.ts
+import { setupTRPC } from "@trpc/next";
+import type { AppRouter } from "@acme/api";
+import { transformer } from "@acme/api/transformer";
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export const trpc = setupTRPC<AppRouter>({
+  config() {
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+      transformer,
+    };
+  },
+  ssr: false,
+});
