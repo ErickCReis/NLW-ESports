ea1d82719109ebe0bde8825cdca6e92fa34ac6ea
ErickCReis <erickcorreareis@gmail.com>
2022-09-17 21:26:33 -0300

    [API] Adding cache

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -6,4 +6,19 @@ import { appRouter, createContext } from "@acme/api";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
+  responseMeta({ paths, type, errors }) {
+    const allPublic = paths && paths.every((path) => path.includes('game'));
+    const allOk = errors.length === 0;
+    const isQuery = type === 'query';
+    if (allPublic && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every second
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      return {
+        headers: {
+          'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+    return {};
+  },
 });
